0929634c3c12cdb65fada8785496d7f81b05f21f
Kayano Oyama <oyama_kayano@yahoo.co.jp>
2023-04-11 21:37:46 -0700

    Add questions

---
+++ survey.js
@@ -0,0 +1,43 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("What's your name? Nicknames are also acceptable :) ", (answer) => {
+  rl.question("What's an activity you like doing? ", (answer1) => {
+    rl.question("What do you listen to while doing that? ", (answer2) => {
+      rl.question(
+        "Which meal is your favourite (eg: dinner, brunch, etc.) ",
+        (answer3) => {
+          rl.question(
+            "What's your favourite thing to eat for that meal? ",
+            (answer4) => {
+              rl.question(
+                "Which sport is your absolute favourite? ",
+                (answer5) => {
+                  rl.question(
+                    "What is your superpower? In a few words, tell us what you are amazing at! ",
+                    (answer6) => {
+                      //
+                      console.log(`Your nickname: ${answer}`);
+                      console.log(`Your favourite activity: ${answer1}`);
+                      console.log(`Your favourite music: ${answer2}`);
+                      console.log(`Your favourite meal${answer3}`);
+                      console.log(`Your favourite food: ${answer4}`);
+                      console.log(`Your favourite sport: ${answer5}`);
+                      console.log(`Your superpower: ${answer6}`);
+
+                      rl.close();
+                    }
+                  );
+                }
+              );
+            }
+          );
+        }
+      );
+    });
+  });
+});
